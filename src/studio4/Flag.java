02f8bd886c6a2aaafd74bf06adcf3cffeca4a026
Emily Shen <Emily Shen@LAPTOP-ISGJANCQ.dhcp.wustl.edu>
2023-02-23 15:34:11 -0600

    our amazing triangle flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,14 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(189,159,191);
+		StdDraw.filledRectangle(0.5,0.5,0.3,0.2);
+		double[] x = {0.4, 0.6,0.5};
+		double[] y = {0.4,0.4, 0.6};
+		StdDraw.setPenRadius(0.05);
+		StdDraw.setPenColor(128,169,191);
+		StdDraw.polygon(x,y);
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,74 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType= in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		
+		
+		
+		if (shapeType.equals("ellipse"))
+		{
+			double p1= in.nextDouble();
+			double p2= in.nextDouble();
+			double p3=in.nextDouble();
+			double p4=in.nextDouble();
+			
+			if (isFilled==true)
+			{
+				
+				StdDraw.setPenColor(red, green, blue);
+				StdDraw.filledEllipse(p1, p2, p3, p4);
+			}
+			else
+			{
+				StdDraw.setPenColor(red, green, blue);
+				StdDraw.ellipse(p1, p2, p3, p4);
+			}
+		}
+		else if (shapeType.equals("rectangle"))
+		{
+			double p1= in.nextDouble();
+			double p2= in.nextDouble();
+			double p3=in.nextDouble();
+			double p4=in.nextDouble();
+			
+			if (isFilled==true)
+			{
+				
+				StdDraw.setPenColor(red, green, blue);
+				StdDraw.filledRectangle(p1, p2, p3, p4);
+			}
+			else
+			{
+				StdDraw.setPenColor(red, green, blue);
+				StdDraw.rectangle(p1, p2, p3, p4);
+			}
+		}
+		else
+		{
+			double p1= in.nextDouble();
+			double p2= in.nextDouble();
+			double p3=in.nextDouble();
+			double p4=in.nextDouble();
+			double p5=in.nextDouble();
+			double p6 = in.nextDouble();
+			
+			double [] x = {p1,p3,p5};
+			double [] y = {p2,p4,p6};
+			if (isFilled==true)
+			{
+				
+				StdDraw.setPenColor(red, green, blue);
+				StdDraw.filledPolygon(x,y);
+			}
+			else
+			{
+				StdDraw.setPenColor(red, green, blue);
+				StdDraw.polygon(x,y);
+			}
+		}
 	}
 }
